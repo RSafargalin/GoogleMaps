0363184eddcc93246e334f74be1235521eb82ada
Ruslan Safargalin <r.safargalin@icloud.com>
2021-08-20 19:37:51 +0500

    Implemented map screen
    Added Google Maps module, tracking user location and setting a mark on the map when updating user location

---
+++ GMaps/Resources/SecretConstants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  GMaps
+//
+//  Created by Ruslan Safargalin on 18.08.2021.
+//
+
+import Foundation
+
+enum SecretConstants {
+    
+    enum APIKey: String {
+        case GoogleMaps = "AIzaSyCZyDO0AsolNUfHDT9xW226r1VCfDrcL7M"
+    }
+    
+}
